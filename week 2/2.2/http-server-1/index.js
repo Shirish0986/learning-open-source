890f41ee061737b085fbb01688719a2edd4b49d0
Shirish0986 <digitalcrunch.z@gmail.com>
2024-09-03 14:29:46 +0530

    learning http-server

--- week 2/2.2/http-server-1/index.js
+++ week 2/2.2/http-server-1/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/handler-route",(req,res)=>{
+  //header,body,query paramater
+  // do machine learning model
+  // res.send("hello")
+  res.json({
+    Fullname:"Shirish",
+    age : 29
+  })
 })
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.listen(port)
+app.listen(port,()=>{
+  console.log("example")
+})
