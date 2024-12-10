62617393ab7171ad8537d6253106a35a3da688ba
Shirish0986 <digitalcrunch.z@gmail.com>
2024-12-11 02:33:57 +0530

    revising JS

---
+++ week 1/revising js till async await,DOM,events,promises,callbacks/3.js
@@ -0,0 +1,29 @@
+for (let i = 0; i < 10; i++) {
+    console.log("shirish");
+
+}
+// ============== even no. 1 to 100   ================
+console.log("=====================")
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+/* ===========Create a game where you start with any random game number. Ask the user to keep
+guessing the game number until the user enters correct value. */
+
+console.log("===========learning strings==========")
+
+let string1="shirish";
+console.log(string1.length);
+console.log(string1[2]);
+console.log(string1.slice(0,2))
+console.log("a,b,c".split(","))
+// ==================================================
+
+let fullname = "shirish"
+let surname ="singh"
+console.log(`@${fullname}${surname}19`)
---
+++ week 1/revising js till async await,DOM,events,promises,callbacks/4.js
@@ -0,0 +1,31 @@
+let heroes = ["superman", "hulk", "mom", "dad"];
+console.log(heroes.length)
+console.log(heroes[1])
+for (let i = 0; i < heroes.length; i++) {
+    console.log(heroes[i]);
+}
+console.log("==================")
+// ===========================new question==========
+
+let marks = [23, 24, 45, 56, 65, 56];
+sum=0
+let avg;
+for (let val of marks) {
+    avg=(sum += val)/marks.length;
+}
+console.log(avg)
+
+console.log("==================================")
+
+// ===================================================
+let newarray=[]
+let items =[250, 645, 300, 900, 50]
+for(let i=0; i<items.length; i++){
+    let discountedrpice=items[i]-(items[i]*0.1);
+    newarray.push(discountedrpice)
+}
+console.log(newarray)
+
+// ======================================================
+
+
