eef212d6f82eba1eb58aa71840f432e4937593fc
Shirish0986 <digitalcrunch.z@gmail.com>
2024-12-10 19:28:51 +0530

    revising JS

---
+++ week 1/revising js till async await,DOM,events,promises,callbacks/2.js
@@ -0,0 +1,12 @@
+/* Get user to input a number using prompt(“Enter a number: ”). Check if the number is a multiple of 5 or not.*/
+
+function n(n) {
+    if (n % 5 == 0) {
+        console.log("its multiply of 5")
+    }
+    else {
+        console.log("not")
+    }
+}
+n(3)    
+// =============================================
